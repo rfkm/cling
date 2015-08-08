1962b7b8da42190d8ab75850bec6212410d1b9e6
Ryo Fukumuro <rkworks@o0o.co>
2015-08-09 08:41:29 +0900

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rkworks/cling "0.1.1-SNAPSHOT"
+(defproject rkworks/cling "0.1.1"
   :description "A Clojure CLI applications library."
   :url "https://github.com/rkworks/cling"
   :license {:name "Eclipse Public License"
